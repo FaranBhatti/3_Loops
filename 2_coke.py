52592de64018cf84c4a6bf421c977db18d2d2598
Faran Bhatti <faran_bhatti@hotmail.com>
2023-03-01 13:29:03 -0800

    Starting coke problem

---
+++ 2_coke.py
@@ -0,0 +1,10 @@
+"""
+File name:                   2_coke.py
+Description:                 Machine sells Coke for 50 cents and only accepts coins in denominations of 25, 10, 5 cents
+                             Once the user inputs atleast 50 cents, output how many cents in change the user is owed.
+                             Assume that the user will only input integers, and ignore any integer that isn't an accepted denomination.
+Created:                     Faran Bhatti
+Edited:                      2023-03-01 by Faran Bhatti (FB)
+Last Edited:                 2023-03-01 by Faran Bhatti (FB)
+"""
+
